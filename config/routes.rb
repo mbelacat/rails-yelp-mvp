997cb5ad1c823f0aba4aef10bb88b4f0a35d7af1
mbela mbemba <mbela.mbemba@gmail.com>
2018-05-23 17:17:53 +0200

    Yelp

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+#get "restaurants", to: "restaurants#index"
+#post "restaurants", to: "restaurants#create"
+#get "restaurants/new", to:"restaurants#new", as: :new
+#get "reviews/new", to:"review#new"
+#post "reviews", to:"review#create"
+#get "restaurants/:id", to:"restaurants#show", as: :task
+#get "restaurants/:id/edit", to:"restaurants#edit", as: :edit
+  resources :restaurants do
+   resources :reviews, only: [ :new, :create]
+  end
 end
+
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    phone_number:  'Buzzy destination for Indian street food in Bombay-style vintage decor.',
+    category:        5,
+  },
+  {
+    name:         'Pizza East',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    phone_number:  'Pizzeria with industrial looks, serving rustic pizza and antipasti.',
+    category:        4,
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!
